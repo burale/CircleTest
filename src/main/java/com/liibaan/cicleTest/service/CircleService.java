c07c4ea2dbad5535c5128a59c1e3ee5459b8bd83
Liibaan <burale2000@hotmail.com>
2019-11-07 15:40:41 +0000

    More tests

--- src/main/java/com/liibaan/cicleTest/service/CircleService.java
+++ src/main/java/com/liibaan/cicleTest/service/CircleService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class CircleService {
 
     public double getArea(int radius){
-        return radius * 2;
+        return radius * 3;
     }
 }
